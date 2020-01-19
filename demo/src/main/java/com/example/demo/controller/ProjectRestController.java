f1f0a9ebda2f8f9ca2558bcd430b1e675ba09582
Dan Cosarca <cosarcadan@gmail.com>
2020-01-19 13:49:40 +0200

    Formatting ProjectRestController

--- demo/src/main/java/com/example/demo/controller/ProjectRestController.java
+++ demo/src/main/java/com/example/demo/controller/ProjectRestController.java
@@ -4,11 +4,8 @@ import com.example.demo.service.ProjectService;
 import com.example.demo.service.dto.ProjectDto;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/project")
@@ -19,16 +16,16 @@ public class ProjectRestController {
 
 
     @GetMapping("/getall")
-    public ResponseEntity<String> getAll(){
+    public ResponseEntity<String> getAll() {
         Gson gson = new Gson();
         String response = gson.toJson(projectService.findAll());
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<String> getById(@PathVariable("id") int id){
+    public ResponseEntity<String> getById(@PathVariable("id") int id) {
         Gson gson = new Gson();
-        if(projectService.findById(id) != null) {
+        if (projectService.findById(id) != null) {
             String response = gson.toJson(projectService.findById(id));
             return ResponseEntity.ok().body(response);
         }
@@ -36,37 +33,37 @@ public class ProjectRestController {
     }
 
     @PostMapping("/add") //optional consumes = "application/json" produces = "application/json"
-    public ResponseEntity<String> saveProject(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<String> saveProject(@RequestBody ProjectDto projectDto) {
         ProjectDto response = projectService.save(projectDto);
-        if(response != null){
+        if (response != null) {
             return ResponseEntity.ok().body("Project saved!");
         }
         return ResponseEntity.badRequest().body("Project save failed!");
     }
 
     @PostMapping("/deactivate")
-    public ResponseEntity<String> deactivateProject(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<String> deactivateProject(@RequestBody ProjectDto projectDto) {
         projectService.deactivate(projectDto);
         return ResponseEntity.ok().body("Project deactivated!");
     }
 
     @PostMapping("/update")
-    public ResponseEntity<String> updateProect(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<String> updateProect(@RequestBody ProjectDto projectDto) {
         ProjectDto response = projectService.update(projectDto);
-        if (response != null){
+        if (response != null) {
             return ResponseEntity.ok().body("Project updated!");
         }
         return ResponseEntity.badRequest().body("Project update failed!");
     }
 
     @PostMapping("/assignCustomer")
-    public ResponseEntity<String> assignCustomer(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<String> assignCustomer(@RequestBody ProjectDto projectDto) {
         projectService.assignCustomer(projectDto);
         return ResponseEntity.ok().body("Project linked to Customer!");
     }
 
     @PostMapping("/assignIndustry")
-    public ResponseEntity<String> assignIndustry(@RequestBody ProjectDto projectDto){
+    public ResponseEntity<String> assignIndustry(@RequestBody ProjectDto projectDto) {
         projectService.assignIndustry(projectDto);
         return ResponseEntity.ok().body("Project linked to Industry"); // Custom Message Possible String.format("Project %s linked to Industry %s",projectDto.getName(), industryService.find...
 
