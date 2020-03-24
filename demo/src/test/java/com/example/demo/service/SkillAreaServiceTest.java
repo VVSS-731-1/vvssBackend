21d07834bf4414a92f8d3c7332b518213f5ccf73
Denis Eneotescu <Deniseneotescu@gmail.com>
2020-03-24 17:12:29 +0200

    findAllSkillArea done

--- demo/src/test/java/com/example/demo/service/SkillAreaServiceTest.java
+++ demo/src/test/java/com/example/demo/service/SkillAreaServiceTest.java
@@ -1,30 +1,47 @@
 package com.example.demo.service;
 
 import com.example.demo.model.SkillArea;
+import com.example.demo.repository.SkillAreaRepository;
 import com.example.demo.service.services.SkillAreaService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class SkillAreaServiceTest {
 
     @Autowired
-    SkillAreaService skillAreaService;
+    SkillAreaService service;
+
+    @MockBean
+    SkillAreaRepository repository;
 
     @Test
     void findAll() {
+        when(repository.findAll()).thenReturn(Stream.of(new SkillArea(), new SkillArea()).collect(Collectors.toList()));
+        assertEquals(2, service.findAll().size());
+    }
 
-        List<SkillArea> skillAreas = skillAreaService.findAll();
+    @Test
+    void findById() {
+        int id = 82;
+        SkillArea skillArea = new SkillArea();
+        skillArea.setId(id);
+        when(repository.findById(id)).thenReturn(Optional.of(skillArea));
+        assertEquals(id, service.findById(id).getId());
 
-        assertEquals(skillAreas.size(), 6);
-
+        when(repository.findById(-1)).thenReturn(null);
+        assertNull(service.findById(-1));
     }
 }
--- demo/src/test/java/com/example/demo/service/UserServiceTest.java
+++ demo/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,65 +1,77 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Project;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.dto.DtoMapping;
+import com.example.demo.service.dto.ProjectDto;
 import com.example.demo.service.dto.UserDTO;
 import com.example.demo.service.services.UserService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class UserServiceTest {
 
-    @InjectMocks
+    @Autowired
     UserService userService;
 
-    @Mock
+    @MockBean
     private UserRepository userRepository;
 
     @Mock
     private DtoMapping dtoMapping;
 
-    private User user;
-
-    private UserDTO userDTO;
-
-    private NullPointerException nullPointerException;
-
-    @Mock
-    private List<User> userList;
-
     @Test
-    private void findAll() {
-        when(dtoMapping.dtoToUser(userDTO)).thenReturn(user);
-        when(userRepository.findAll()).thenReturn(userList);
+    public void findAll() {
+        when(userRepository.findAll()).thenReturn(Stream.of(new User(), new User()).collect(Collectors.toList()));
+        Assert.assertEquals(2, userService.findAll().size());
     }
 
     @Test
-    private void save() {
+    public void findById() {
+        int id = 89;
+        when(userRepository.findById(id)).thenReturn(Optional.of(new User()));
+        Assert.assertEquals(Optional.of(new User()), userService.findById(id));
+    }
+
+    @Test
+    public void save() {
+        UserDTO userDTO = getUserDTO();
         when(userService.save(userDTO)).thenReturn(userDTO);
-        when(userService.save(null)).thenThrow(nullPointerException);
+        Assert.assertEquals(userDTO, userDTO);
     }
 
-    @Test
-    private void findByName() {
-        when(userService.findByName("test")).thenReturn(userDTO);
-        when(userService.findByName("")).thenReturn(null);
-        when(userService.findByName(null)).thenThrow(nullPointerException);
-    }
-
-    @Test
-    private void login() {
-        when(userService.login("test", "password")).thenReturn(userDTO);
-        when(userService.login("","")).thenReturn(null);
-        when(userService.login(null,null)).thenThrow(nullPointerException);
+    public UserDTO getUserDTO() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setCounter(0);
+        userDTO.setAdmin(false);
+        userDTO.setPassword("password");
+        userDTO.setStatus(true);
+        userDTO.setFirstName("paul");
+        userDTO.setLastName("paul");
+        userDTO.setProjects(Stream.of(new ProjectDto()).collect(Collectors.toList()));
+        userDTO.setSupervising(Stream.of(new UserDTO()).collect(Collectors.toSet()));
+        userDTO.setSupervisor(new UserDTO());
+        userDTO.setUsername("hatz");
+        userDTO.setEmail("myEmail@email.com");
+        return userDTO;
     }
 }
